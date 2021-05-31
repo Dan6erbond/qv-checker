bb406d90a9dbb65223ae9dd33bcf2b16ff45dc3c
RaviAnand Mohabir <moravrav@gmail.com>
2021-05-31 15:53:26 +0200

    ✨ Create Netlify serverless function for results

---
+++ netlify/functions/results.ts
@@ -0,0 +1,32 @@
+import { Handler } from "@netlify/functions";
+import fetch from "node-fetch";
+
+const handler: Handler = async function (event) {
+  const { ahvNr, birthdate } = JSON.parse(event.body);
+
+  const res = await fetch(
+    "https://www.ag.ch/app/qvserviceapi/services/qv_info/kandidat",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        ahvNr: ahvNr,
+        geburtsdatum: birthdate,
+      }),
+    },
+  );
+
+  const json = await res.json();
+
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+    body: JSON.stringify(json),
+  };
+};
+
+export { handler };
