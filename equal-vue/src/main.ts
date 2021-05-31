09e7c12e794ac8da8f0867a572f8de2219528996
RaviAnand Mohabir <moravrav@gmail.com>
2021-05-31 17:19:50 +0200

    ➕ Add `equal-vue`

--- equal-vue/src/main.ts
+++ equal-vue/src/main.ts
@@ -1,4 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import Equal from "equal-vue";
+import "../node_modules/equal-vue/dist/style.css";
 
-const app = createApp(App).mount("#app");
+const app = createApp(App).use(Equal).mount("#app");
