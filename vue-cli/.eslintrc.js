fe0bd4a9c2aabd716b5a93fdc6a663bd10a5c0e6
Dan6erbond <moravrav@gmail.com>
2021-06-01 09:10:23 +0200

    🔧 Update ESLint and Prettier configs

--- vue-cli/.eslintrc.js
+++ vue-cli/.eslintrc.js
@@ -16,5 +16,12 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "@typescript-eslint/interface-name-prefix": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/no-unused-vars": ["off", { argsIgnorePattern: "^_" }],
+    "prettier/prettier": ["error", {}, { usePrettierrc: true }],
   },
 };
