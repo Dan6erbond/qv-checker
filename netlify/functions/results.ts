7dab03fbea5b2017a69e9afa395f9ebbcc79b057
Dan6erbond <moravrav@gmail.com>
2021-06-01 10:43:57 +0200

    🎨 Add CORS headers to Netlify endpoint instead of global

--- netlify/functions/results.ts
+++ netlify/functions/results.ts
@@ -22,6 +22,9 @@ const handler: Handler = async function (event) {
 
   return {
     statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
     body: JSON.stringify(json),
   };
 };
